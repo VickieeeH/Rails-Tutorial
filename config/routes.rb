278a0309b1058ac05d2d8c8ad0120f98a2aa5968
buyifly <buyifly@live.com>
2016-09-21 11:34:41 +1200

    Basic Login

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
+
   get 'users/new'
 
   root 'static_page#home'
@@ -6,7 +8,9 @@ Rails.application.routes.draw do
   get  '/about',   to: 'static_page#about'
   get  '/contact', to: 'static_page#contact'
   get  '/signup',  to: 'users#new'
-  post '/signup',  to: 'users#create'
+  get  '/login',   to: 'sessions#new'
+  post '/login',   to: 'sessions#create'
+  delete '/logout',to: 'sessions#destroy'
 
   resources :users
 end
