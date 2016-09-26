1582185aac341d56761745059b8112cec68b06c8
buyifly <buyifly@live.com>
2016-09-26 17:52:02 +1300

    Fixed text

--- test/integration/micoposts_interface_test.rb
+++ test/integration/micoposts_interface_test.rb
@@ -23,12 +23,6 @@ class MicropostsInterfaceTest < ActionDispatch::IntegrationTest
     assert_redirected_to root_url
     follow_redirect!
     assert_match content, response.body
-    # Delete post
-    assert_select 'a', text: 'delete'
-    first_micropost = @user.microposts.paginate(page: 1).first
-    assert_difference 'Micropost.count', -1 do
-      delete micropost_path(first_micropost)
-    end
     # Visit different user (no delete links)
     get user_path(users(:archer))
     assert_select 'a', text: 'delete', count: 0
